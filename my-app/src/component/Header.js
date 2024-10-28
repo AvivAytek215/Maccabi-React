023c5e23094617a7156168b56e7b6f7e93b10395
LidorAnfinger <lidoranf@gmail.com>
2024-10-28 17:06:10 +0200

    header fixed to look the same for every page

--- my-app/src/component/Header.js
+++ my-app/src/component/Header.js
@@ -17,38 +17,32 @@ const Header = ({ isLoggedIn }) => {
 
     return (
         <header className="header">
-            <div className="header-container">
-                <div className="header-content">
-                    {/* Left side - Logos */}
-                    <div className="header-logos">
-                        <div className="desktop-nav">
-                            <HamburgerMenu/>
-                        </div>
-                        <img
-                            src={`${process.env.PUBLIC_URL}/Photos/Maccabi React.png`}
-                            alt="Team Logo"
-                            className="team-logo"
-                            onClick={handleLogoClicked}
-                        />
-                        <img
-                            src={`${process.env.PUBLIC_URL}/Photos/sponsor.png`} 
-                            className="sponsor-logo"
-                            alt="sponsor logo"
-                        />
-                        
-                    </div>
-
-                    {/* Right side - Login/Profile */}
-                    <div className="auth-section">
-                        {isLoggedIn ? (
-                            <div className="profile-button">Profile</div>
-                        ) : (
-                            <button className="login-button" onClick={handleLoginClick}>
+            <div className="header-left">
+                <div className="hamburger-menu">
+                    <HamburgerMenu/>
+                </div>
+                <img
+                    src={`${process.env.PUBLIC_URL}/Photos/sponsor.png`} 
+                    className="sponsor-logo"
+                    alt="sponsor logo"
+                />   
+                <img
+                    src={`${process.env.PUBLIC_URL}/Photos/Maccabi React.png`}
+                    alt="Team Logo"
+                    className="team-logo"
+                    onClick={handleLogoClicked}
+                />
+            </div>
+            <h1 className="team-name-header">Maccabi React Official Website</h1>
+            {/* Right side - Login/Profile */}
+            <div className="auth-section">
+                {isLoggedIn ? (
+                    <div className="profile-button">Profile</div>
+                ) : (
+                    <button className="login-button" onClick={handleLoginClick}>
                                 Login
                             </button>
                         )}
-                    </div>
-                </div>
             </div>
         </header>
     );
